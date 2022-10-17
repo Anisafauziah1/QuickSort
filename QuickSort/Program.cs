58409ab4632c4835443d2755359b271a142b29e5
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-10-17 17:02:16 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // Number of elements in array
+        private int n;
+
+
+        void read()
+        {
+            while (true) 
+            {
+                Console.Write("Enter the nimber of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+            
+            }
+         
+        }
+    }
